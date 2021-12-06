cce9c3cf94a4e6c3730efda6d23f25c570664bfc
james-muriithi <muriithijames556@gmail.com>
2021-12-06 23:13:37 +0300

    added counter component

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss'],
+})
+export class CounterComponent implements OnInit {
+  counter: number = 0;
+
+  constructor() {}
+
+  increment(): void {
+    this.counter++;
+  }
+
+  ngOnInit(): void {}
+}
